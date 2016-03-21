f25ca022951ebf2cdd84749d24b8ab9ff3ff398a
Noah Kittleson and Nick Lyman <nick@>
2016-03-21 16:01:19 -0700

    added user input to add, subtract, multiply or divide

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,20 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
+var text = prompt("What calcution you like to do?")
 var number1 = parseInt(prompt("Enter a number: "));
 var number2 = parseInt(prompt("Enter another number: "));
-var result = divide(number1, number2);
-alert (result);
+
+
+if (text == "multiply") {
+	alert(multiply(number1, number2));
+}
+if (text == "add") {
+	alert(add(number1, number2));
+}
+if (text == "subtract"){
+	alert(subtract(number1, number2));
+}
+if (text == "divide"){
+	alert(divide(number1, number2));
+}
